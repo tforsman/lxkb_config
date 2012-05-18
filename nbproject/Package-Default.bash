022ec4c7176c8e26d12dd6d2703eb416dd200545
Alexis López Zubieta <azubieta@estudiantes.uci.cu>
2012-05-17 22:26:50 -0400

    Adding autotools.

--- nbproject/Package-Default.bash
+++ nbproject/Package-Default.bash
@@ -7,14 +7,14 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=GNU-Linux-x86
-CND_CONF=Release
+CND_CONF=Default
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lxkb_config
+OUTPUT_PATH=src/lxkb_config
 OUTPUT_BASENAME=lxkb_config
-PACKAGE_TOP_DIR=lxkbconfig/
+PACKAGE_TOP_DIR=lxkb-config/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/lxkbconfig/bin"
+makeDirectory "${NBTMPDIR}/lxkb-config/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/lxkbconfig.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/lxkb-config.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/lxkbconfig.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/lxkb-config.tar *
 checkReturnCode
 
 # Cleanup
